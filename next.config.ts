f6a7dde79fd00488857da3d3b4720c8e84e429fa
arcaniussainey <br.3cow@gmail.com>
2025-06-19 18:47:21 -0400

    Added some stuff

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ const withMDX = require('@next/mdx')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/',
+  basePath: '',
+  trailingSlash: true, // Required for GitHub Pages to serve correctly
   output: 'export', // Enable static export
   distDir: 'out',
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'], // Include MDX files as pages
