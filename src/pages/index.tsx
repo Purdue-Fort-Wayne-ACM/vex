f605fc8bb8319102d8f1bc6b1b48f50748bbc22e
Treasure-Chandler <tchandler1211@gmail.com>
2025-06-20 09:57:21 -0400

    Proofreading pages, added logic-diagrams-intro to the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 // Replace this with catalog of other pages. 
 export default function App(){
   return (<><body>
-    <a href="/vexexp/color-detection">Color Detection</a>
+    <a href="/vexexp/color-detection">Color Detection</a><br />
+    <a href="/vexexp/logic-diagrams-intro">Logic Diagrams Intro</a>
   </body></>)
 }
