503f7b62420c377dc05f79e56c8888358f779eaa
Treasure-Chandler <tchandler1211@gmail.com>
2025-06-21 15:54:12 -0400

    Added opencv-image-processing to the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function App(){
   return (<><body>
     <a href="/vexexp/color-detection">Color Detection</a><br />
     <a href="/vexexp/logic-diagrams-intro">Logic Diagrams - Intro</a><br/ >
-    <a href="/vexexp/matplotlib-basics-pyplot">MatPlotLib - Basics & Pyplot</a>
+    <a href="/vexexp/matplotlib-basics-pyplot">MatPlotLib - Basics & Pyplot</a><br/ >
+    <a href="/vexexp/opencv-image-processing">OpenCV - Image Processing</a>
   </body></>)
 }
