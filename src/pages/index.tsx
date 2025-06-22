2dc88385f0da835c2d67607f63dc1df80c9f4df5
Treasure-Chandler <tchandler1211@gmail.com>
2025-06-21 21:37:48 -0400

    Started the conversion of the psuedo-code-intro page; have not made it accessible in the index as of yet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function App(){
     <a href="/vexexp/color-detection">Color Detection</a><br />
     <a href="/vexexp/logic-diagrams-intro">Logic Diagrams - Intro</a><br/ >
     <a href="/vexexp/matplotlib-basics-pyplot">MatPlotLib - Basics & Pyplot</a><br/ >
-    <a href="/vexexp/opencv-image-processing">OpenCV - Image Processing</a>
+    <a href="/vexexp/opencv-image-processing">OpenCV - Image Processing</a><br/ >
+    {/* <a href="/vexexp/psuedo-code-intro">Psuedocode - Intro</a> */}
   </body></>)
 }
